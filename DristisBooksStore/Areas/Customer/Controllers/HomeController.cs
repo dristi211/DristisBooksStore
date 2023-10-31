88a7bb2751bd0f4705918689fe5c5847cdaa3044
dristi211 <122821577+dristi211@users.noreply.github.com>
2023-10-30 22:52:19 -0400

    Modify the HomeController and Move Home to Views

--- DristisBooksStore/Areas/Customer/Controllers/HomeController.cs
+++ DristisBooksStore/Areas/Customer/Controllers/HomeController.cs
@@ -7,7 +7,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace DristisBooksStore.Controllers
+namespace DristisBooksStore.Area.Customer.Controllers
 {
     [Area("Customer")]
     public class HomeController : Controller
