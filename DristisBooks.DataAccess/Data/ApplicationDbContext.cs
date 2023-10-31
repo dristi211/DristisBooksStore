0f619c7dc3d0292436c0505afca64805c170b486
dristi211 <122821577+dristi211@users.noreply.github.com>
2023-10-30 21:41:23 -0400

    Modify the namespace to reflect the project

--- DristisBooks.DataAccess/Data/ApplicationDbContext.cs
+++ DristisBooks.DataAccess/Data/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace DristisBooksStore.Data
+namespace DristisBooksStore.DataAccess.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
