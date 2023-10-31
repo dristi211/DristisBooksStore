c5acdea57e848221c2b66601cc1c49bf97f34fa8
dristi211 <122821577+dristi211@users.noreply.github.com>
2023-10-30 22:45:14 -0400

    Edit the HomeController.cs to explicitly define that the controller is in the Customer Area [Area("Customer")]

--- DristisBooksStore/Areas/Customer/Controllers/HomeController.cs
+++ DristisBooksStore/Areas/Customer/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 
 namespace DristisBooksStore.Controllers
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
