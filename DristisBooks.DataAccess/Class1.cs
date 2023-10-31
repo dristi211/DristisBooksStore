c68fcb4eed34792d0d2cd71ea6e5dc1cf2dcd402
dristi211 <122821577+dristi211@users.noreply.github.com>
2023-10-30 21:42:33 -0400

    Deleted the default Class1.cs file in the project

--- DristisBooks.DataAccess/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace DristisBooks.DataAccess
-{
-    public class Class1
-    {
-    }
-}
--- DristisBooks.Utility/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace DristisBooks.Utility
-{
-    public class Class1
-    {
-    }
-}
