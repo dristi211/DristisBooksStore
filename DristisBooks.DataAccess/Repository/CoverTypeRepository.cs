c9b7c5d454d57687fbc2c789a008cd82884bdacf
W0811195 <W0811195@ADL-A1201H-13>
2023-11-15 10:20:26 -0500

    Created the CoverType cs file in the DristisBooksStore.Model folder

---
+++ DristisBooks.DataAccess/Repository/CoverTypeRepository.cs
@@ -0,0 +1,38 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+*/
+using System.Threading.Tasks;
+using DristisBooks.DataAccess.Repository.IRepository;
+using DristisBooks.Models;
+using DristisBooksStore.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DristisBooks.DataAccess.Repository
+{
+  /*public class CoverTypeRepository : Repository<CoverType>, ICoverTypeRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CoverTypeRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(CoverType coverType)
+        {
+            var objFromDb = _db.CoverTypes.FirstOrDefault(s => s.Id == coverType.Id);
+            if (objFromDb != null)  //save changes if not null
+            {
+                objFromDb.Name = coverType.Name;
+                //_db.SaveChanges();                  //as instructed to remove
+            }
+        }
+
+    }*/
+}
+
